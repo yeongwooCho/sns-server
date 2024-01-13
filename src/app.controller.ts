bc6b614c7d2ff5b079c836497986b0ea94e5f613
yeongwoo <jyy0223@naver.com>
2024-01-13 19:18:51 +0900

    [refactor] posts resource

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-@Controller('/post')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('post')
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '수리하는 민지',
-      likeCount: 12345,
-      commentCount: 24139,
-    };
-  }
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '수리하는 민지',
+      likeCount: 12345,
+      commentCount: 24139,
+    };
+  }
 }
