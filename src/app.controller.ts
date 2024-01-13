e3c0ef258ec9b95b68e483f7e56e4c4ae5fb4682
yeongwoo <jyy0223@naver.com>
2024-01-13 19:00:48 +0900

    [add] controller decorator, method decorator를 이용한 path 지정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('/post')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('post')
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '수리하는 민지',
+      likeCount: 12345,
+      commentCount: 24139,
+    };
   }
 }
