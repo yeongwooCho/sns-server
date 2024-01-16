36e21d991b2dcd0b0c7942ac946dbb37b27ccc2c
yeongwoo <jyy0223@naver.com>
2024-01-16 17:41:41 +0900

    [add] post entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmUserOption } from '../my_settings';
+import { PostsModel } from './posts/entities/posts.entity';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { typeOrmUserOption } from '../my_settings';
       password: typeOrmUserOption.password,
       database: 'postgres',
       // 데이터 베이스와 연동될 모델이 추가된다.
-      entities: [],
+      entities: [PostsModel],
       // NestJS 에서 작성하는 typeorm 코드와
       // database sync 를 자동으로 맞출 것인가?
       // 데이터의 구조가 갑자기 바뀔 수 있기에
