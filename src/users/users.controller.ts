5327b867e7a4e4aea4c00c988066ee9d17217d43
yeongwoo <jyy0223@naver.com>
2024-01-19 00:35:28 +0900

    [add] users의 controller, service의 CRUD 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.usersService.getUserById(+id);
+  }
+
+  @Post()
+  postUser(
+    @Body('nickname') nickname: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.usersService.createUser(nickname, email, password);
+  }
+
+  @Patch(':id')
+  patchUser(
+    @Param('id') id: string,
+    @Body('nickname') nickname?: string,
+    @Body('email') email?: string,
+    @Body('password') password?: string,
+  ) {
+    return this.usersService.updateUser(+id, nickname, email, password);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(+id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UsersModel } from './entities/users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly usersRepository: Repository<UsersModel>,
+  ) {}
+
+  async getAllUsers() {
+    return await this.usersRepository.find();
+  }
+
+  async getUserById(id: number) {
+    return await this.usersRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async createUser(nickname: string, email: string, password: string) {
+    const newUser = this.usersRepository.create({
+      nickname,
+      email,
+      password,
+    });
+
+    return await this.usersRepository.save(newUser);
+  }
+
+  async updateUser(
+    id: number,
+    nickname?: string,
+    email?: string,
+    password?: string,
+  ) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (nickname) {
+      user.nickname = nickname;
+    }
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+
+    return await this.usersRepository.save(user);
+  }
+
+  async deleteUser(id: number) {
+    await this.usersRepository.delete(id);
+
+    return id;
+  }
+}
