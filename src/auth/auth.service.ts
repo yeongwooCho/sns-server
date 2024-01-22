969c9fc0deb2934d9cba79d502f9e0f531575d2d
yeongwoo <jyy0223@naver.com>
2024-01-22 17:42:01 +0900

    [add] 토큰 로직 주석으로 정리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,6 +14,37 @@ export class AuthService {
   ) {}
 
   /**
+   * 토큰을 사용하게 되는 방식
+   *
+   * 1) 사용자가 로그인 또는 회원가입을 진행하면
+   * accessToken 과 refreshToken 을 발급 받는다.
+   *
+   * 2) 로그인 할때는 Basic 토큰과 함께 요청을 보낸다.
+   * Basic 토큰은 '이메일:비밀번호'를 base64로 인코딩한 형태이다.
+   * 예) {authorization: 'Basic {token}'}
+   *
+   * 3) 아무나 접근할 수 없는 정보 (private route)를 접근 할때는
+   * accessToken 을 Header 에 추가해서 요청과 함께 보낸다.
+   * 예) {authorization: 'Bearer {token}'}
+   *
+   * 4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해
+   * 현재 요청을 보낸 사용자가 누구인지 알 수 있다.
+   * JWT 안에 id, email 이 있기에 사용자가 누군지 알 수 있다.
+   * 예) 현재 로그인한 사용자가 작성한 포스트만 가져오려면
+   * 토큰의 sub 값에 입력되어 있는 사용자의 포스트만 따로 필터링 할 수 있다.
+   * 특정 사용자의 토큰이 없다면 다른 사용자의 데이터를 접근 못한다.
+   *
+   * 5) 모든 토큰은 만료 기간이 있다. 만료기간이 지나면 새로 토큰을 발급 받아야 한다.
+   * 그렇지 않으면 jwtService.verify() 에서 인증 통과가 안 된다.
+   * 그러니 access 토큰을 새로 발급 받을 수 있는 /auth/token/access 와
+   * refresh 토큰을 새로 발급 받을 수 있는 /auth/token/refresh 가 필요하다.
+   * 기획에 따라 refresh 는 재발급 안 되야 하는 경우도 있지만 우리는 한다.
+   *
+   * 6) 토큰이 만료되면 각각의 토큰을 새로 발급 받을 수 있는 엔드포인트에 요청을 해서
+   * 새로운 토큰을 발급받고 새로운 토큰을 사용해서 private route 에 접근한다.
+   */
+
+  /**
    * 우리가 만드려는 기능
    *
    * 1) registerWithEmail
