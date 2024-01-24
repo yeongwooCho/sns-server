25f59fb386aa0b789a973c5ecf560f30fb57beb4
yeongwoo <jyy0223@naver.com>
2024-01-24 12:17:27 +0900

    [add] resource common

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { PostsModel } from './posts/entities/posts.entity';
 import { UsersModule } from './users/users.module';
 import { UsersModel } from './users/entities/users.entity';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { AuthModule } from './auth/auth.module';
     PostsModule,
     UsersModule,
     AuthModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
