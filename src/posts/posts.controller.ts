d3b25e9d8ad320c579b3d412163e3f97eee17ee4
yeongwoo <jyy0223@naver.com>
2024-01-25 16:17:37 +0900

    [add] custom parameter decorator 를 통해 API에서 요청객체를 열어 user를 확인하지 않고 user를 주입 받도록 수정

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -13,6 +13,8 @@ import {
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { AccessTokenGuard } from '../auth/guard/bearer-token.guard';
+import { User } from "../users/decorator/user.decorator";
+import { UsersModel } from "../users/entities/users.entity";
 
 @Controller('posts')
 export class PostsController {
@@ -34,13 +36,11 @@ export class PostsController {
   @Post()
   @UseGuards(AccessTokenGuard)
   postPost(
-    @Request() req: any,
+    @User() user: UsersModel,
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const authorId = req.user.id;
-
-    return this.postsService.createPost(authorId, title, content);
+    return this.postsService.createPost(user.id, title, content);
   }
 
   // 4) PATCH /posts/:id
---
+++ src/users/decorator/user.decorator.ts
@@ -0,0 +1,20 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const User = createParamDecorator((data, context: ExecutionContext) => {
+  const req = context.switchToHttp().getRequest();
+
+  const user = req.user;
+
+  if (!user) {
+    throw new InternalServerErrorException(
+      'User 데코레이터는 AccessTokenGuard와 함께 사용해야 합니다.',
+      // 'Request에 user 프로퍼티가 존재하지 않습니다.',
+    );
+  }
+
+  return user;
+});
