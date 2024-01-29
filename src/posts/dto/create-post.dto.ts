086e6b724a68f450477c1e3e2c059982488ff106
yeongwoo <jyy0223@naver.com>
2024-01-29 20:00:53 +0900

    [add] dto error message

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
+  @IsString({
+    message: 'title 은 string 타입을 입력 해줘야 합니다.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'content 는 string 타입을 입력 해줘야 합니다.',
+  })
   content: string;
 }
