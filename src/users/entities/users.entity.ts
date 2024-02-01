2f22eb3d07ee2b131fb166283b0752c49e5ee678
yeongwoo <jyy0223@naver.com>
2024-02-01 21:20:58 +0900

    [add] class-transformer expose annotation 을 이용해 실제 property 가 아닌 값을 노출시키는 기능 구현

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -5,7 +5,7 @@ import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from '../../common/validation-message/length-validation.message';
 import { stringValidationMessage } from '../../common/validation-message/string-validation.message';
 import { emailValidationMessage } from '../../common/validation-message/email-validation.message';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 export enum RolesEnum {
   USER = 'USER',
@@ -25,6 +25,11 @@ export class UsersModel extends BaseModel {
     message: lengthValidationMessage,
   })
   nickname: string;
+
+  @Expose()
+  get nicknameAndEmail() {
+    return this.nickname + '/' + this.email;
+  }
 
   @Column({
     unique: true,
