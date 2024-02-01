35c52e1186eec2d332be59ffe4eb8c0d47f4166a
yeongwoo <jyy0223@naver.com>
2024-02-01 21:08:11 +0900

    [add] class-transformer Exclude annotation 을 이용한 직렬화 역직렬화 과정에서 제외될 property 를 제어

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -5,6 +5,7 @@ import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from '../../common/validation-message/length-validation.message';
 import { stringValidationMessage } from '../../common/validation-message/string-validation.message';
 import { emailValidationMessage } from '../../common/validation-message/email-validation.message';
+import { Exclude } from 'class-transformer';
 
 export enum RolesEnum {
   USER = 'USER',
@@ -45,6 +46,22 @@ export class UsersModel extends BaseModel {
   })
   @Length(3, 8, {
     message: lengthValidationMessage,
+  })
+  /**
+   * Request
+   * frontend -> backend
+   * plain object (JSON) -> class instance(dto)
+   *
+   * Response
+   * backend -> frontend
+   * class instance(dto) -> plain object (JSON)
+   *
+   * toClassOnly -> class instance 로 변환될때만(요청)
+   * toPlainOnly -> plain object 로 변환될때만(응답)
+   * 옵션 입력 안하면 둘다 적용되는데 우리는 비밀번호를 받아야 한다.
+   */
+  @Exclude({
+    toPlainOnly: true,
   })
   password: string;
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
   Param,
   Patch,
-  Post,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 
@@ -14,6 +15,15 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  // @UseInterceptors(ClassSerializerInterceptor)
+  /**
+   * serialization(직렬화)
+   * -> 현재 시스템에서 사용되는 NestJS 데이터 구조를 다른 시스템에서도
+   * 쉽게 사용할 수 있는 포맷으로 변환
+   * -> class의 object에서 JSON 포맷으로 변환
+   *
+   * deserialization(역직렬화)
+   */
   getUsers() {
     return this.usersService.getAllUsers();
   }
