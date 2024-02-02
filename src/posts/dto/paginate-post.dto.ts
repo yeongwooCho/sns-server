e43f07017191a224b566678fad0cd9a662b652b4
yeongwoo <jyy0223@naver.com>
2024-02-02 16:14:09 +0900

    [add] PaginatePostDto 생성

---
+++ src/posts/dto/paginate-post.dto.ts
@@ -0,0 +1,21 @@
+import { IsIn, IsNumber, IsOptional } from 'class-validator';
+
+export class PaginatePostDto {
+  // 이전 마지막 데이터의 ID
+  // 이 프로퍼티에 입력된 ID 보다 높은 ID 부터 값을 가져오기
+  @IsNumber()
+  @IsOptional()
+  // 없으면 0으로 처리
+  where__id_more_than?: number;
+
+  // 정렬
+  // createAt -> 생성된 시간의 내림차/오름차 순으로 정렬;
+  @IsIn(['ASC', 'DESC'])
+  @IsOptional()
+  order__createdAt: 'ASC' | 'DESC' = 'ASC'; // 일단 오름차순만 진행
+
+  // 몇개의 데이터를 응답으로 받을지
+  @IsNumber()
+  @IsOptional()
+  take: number = 20;
+}
