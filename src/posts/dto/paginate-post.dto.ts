d622a2cc144c065b2a74fafa34081673ba4b58ff
yeongwoo <jyy0223@naver.com>
2024-02-02 18:03:22 +0900

    [add] enableImplicitConversion 으로 class-validator 를 이용해 class-transformer Type 을 대체하도록 구현

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -1,8 +1,10 @@
 import { IsIn, IsNumber, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class PaginatePostDto {
   // 이전 마지막 데이터의 ID
   // 이 프로퍼티에 입력된 ID 보다 높은 ID 부터 값을 가져오기
+  // @Type(() => Number)
   @IsNumber()
   @IsOptional()
   // 없으면 0으로 처리
