5196e417bc146bb2cc63e05b7f69a88776544493
yeongwoo <jyy0223@naver.com>
2024-03-27 15:29:16 +0900

    [feat] pagePaginate, cursorPaginate 분기

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { BasePaginationDto } from './dto/base-pagination.dto';
+import { FindManyOptions, Repository } from 'typeorm';
+import { BaseModel } from './entity/base.entity';
 
 @Injectable()
-export class CommonService {}
+export class CommonService {
+  paginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string,
+  ) {
+    if (dto.page) {
+      return this.pagePaginate(dto, repository, overrideFindOptions);
+    } else {
+      return this.cursorPaginate(dto, repository, overrideFindOptions, path);
+    }
+  }
+
+  private async pagePaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+  ) {}
+  private async cursorPaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string,
+  ) {}
+}
