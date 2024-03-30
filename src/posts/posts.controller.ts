78d43c9b8c435595023b863823dfe27646fdc1b0
yeongwoo <jyy0223@naver.com>
2024-03-30 19:29:59 +0900

    [feat] QueryRunner decorator 적용: typeorm과 이름 겹침으로 수정, service.getPostById 도 transaction 으로 묶이게 수정

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -23,6 +23,9 @@ import { ImageModelType } from '../common/entity/image.entity';
 import { DataSource } from 'typeorm';
 import { PostsImagesService } from './image/Images.service';
 import { LogInterceptor } from '../common/interceptor/log.interceptor';
+import { TransactionInterceptor } from '../common/interceptor/transaction.interceptor';
+import { QueryRunner } from '../common/decorator/query-runner.decorator';
+import { QueryRunner as QR } from 'typeorm';
 
 @Controller('posts')
 export class PostsController {
@@ -56,51 +59,27 @@ export class PostsController {
   // 3) Post /posts
   @Post()
   @UseGuards(AccessTokenGuard)
+  @UseInterceptors(TransactionInterceptor)
   async postPost(
     @User('id') userId: number,
+    @QueryRunner() qr: QR,
     @Body() body: CreatePostDto, // DTO - Data Transfer Object
   ) {
-    // transaction 과 관련된 모든 쿼리를 담당할 query runner 를 생성한다.
-    const qr = this.dataSource.createQueryRunner();
+    const post = await this.postsService.createPost(userId, body, qr);
 
-    // query runner 에 연결한다. 그럼 transaction 을 시작할 수 있다.
-    await qr.connect();
-
-    // query runner 에서 transaction 을 시작한다.
-    // 이 시점부터 같은 query runner 를 사용하면 transaction 안에서 DB action 을 실행할 수 있다.
-    await qr.startTransaction();
-
-    // 로직 실행
-    try {
-      const post = await this.postsService.createPost(userId, body, qr);
-
-      // transaction test
-      // throw new InternalServerErrorException('테스트');
-
-      for (let i = 0; i < body.images.length; i++) {
-        await this.postsImagesService.createPostImage(
-          {
-            post,
-            order: i,
-            path: body.images[i],
-            type: ImageModelType.POST_IMAGE,
-          },
-          qr,
-        );
-      }
-
-      // transaction 을 commit 한다.
-      await qr.commitTransaction();
-
-      return this.postsService.getPostById(post.id);
-    } catch (e) {
-      // transaction 을 rollback 한다.
-      await qr.rollbackTransaction();
-      throw e;
-    } finally {
-      // query runner 를 release 한다.
-      await qr.release();
+    for (let i = 0; i < body.images.length; i++) {
+      await this.postsImagesService.createPostImage(
+        {
+          post,
+          order: i,
+          path: body.images[i],
+          type: ImageModelType.POST_IMAGE,
+        },
+        qr,
+      );
     }
+
+    return this.postsService.getPostById(post.id, qr);
   }
 
   // 4) PATCH /posts/:id
