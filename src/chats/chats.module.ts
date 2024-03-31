ddf8af9c5d064774517345696793be7af5b22eb1
yeongwoo <jyy0223@naver.com>
2024-03-31 22:20:10 +0900

    [feat] ChatsModel TypeOrm을 사용하기 위한 repository 셋팅

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
 import { ChatsGateway } from './chats.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatsModel } from './entity/chats.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ChatsModel])],
   controllers: [ChatsController],
   providers: [ChatsGateway, ChatsService],
 })
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ChatsModel } from './entity/chats.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ChatsService {}
+export class ChatsService {
+  constructor(
+    @InjectRepository(ChatsModel)
+    private readonly chatsRepository: Repository<ChatsModel>,
+  ) {}
+}
