a6c2db599a99cdce89b0d36e4fb795b7e19ea093
yeongwoo <jyy0223@naver.com>
2024-04-01 01:14:06 +0900

    [feat] message createMessage 구현: dto에서 객체를 받는 경우 id 값만 받기위해 body에 직접 선언한다.

--- src/chats/messages/messages.service.ts
+++ src/chats/messages/messages.service.ts
@@ -4,6 +4,7 @@ import { FindManyOptions, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CommonService } from '../../common/common.service';
 import { PaginateMessageDto } from './dto/paginate-message.dto';
+import { CreateMessagesDto } from './dto/create-messages.dto';
 
 @Injectable()
 export class ChatsMessagesService {
@@ -12,6 +13,24 @@ export class ChatsMessagesService {
     private readonly messagesRepository: Repository<MessagesModel>,
     private readonly commonService: CommonService,
   ) {}
+
+  async createMessage(dto: CreateMessagesDto) {
+    const message = await this.messagesRepository.save({
+      chat: {
+        id: dto.chatId,
+      },
+      author: {
+        id: dto.authorId,
+      },
+      message: dto.message,
+    });
+
+    return this.messagesRepository.findOne({
+      where: {
+        id: message.id,
+      },
+    });
+  }
 
   async paginateMessages(
     dto: PaginateMessageDto,
