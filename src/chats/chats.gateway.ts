804fd4e42102ff4e12832e50f6af5ca319440483
yeongwoo <jyy0223@naver.com>
2024-03-31 23:44:50 +0900

    [feat] 채팅방 생성 API 를 Socketio로 구현: users와 chats를 저장

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -7,17 +7,26 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { ChatsService } from './chats.service';
 
 @WebSocketGateway({
   // ws://localhost:3000/chats
   namespace: 'chats',
 })
 export class ChatsGateway implements OnGatewayConnection {
+  constructor(private readonly chatsService: ChatsService) {}
+
   @WebSocketServer()
   server: Server;
 
   handleConnection(socket: Socket) {
     console.log(`on connect called: ${socket.id}`);
+  }
+
+  @SubscribeMessage('create_chat')
+  async createChat(@MessageBody() data: CreateChatDto) {
+    const chat = await this.chatsService.createChat(data);
   }
 
   @SubscribeMessage('enter_chat')
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ChatsModel } from './entity/chats.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateChatDto } from './dto/create-chat.dto';
 
 @Injectable()
 export class ChatsService {
@@ -9,4 +10,17 @@ export class ChatsService {
     @InjectRepository(ChatsModel)
     private readonly chatsRepository: Repository<ChatsModel>,
   ) {}
+
+  async createChat(dto: CreateChatDto) {
+    const chat = await this.chatsRepository.save({
+      // dto.users.map((userId) => ({ id: userId })) -> [{ id: 1 }, { id: 2 }]
+      users: [...dto.userIds.map((id) => ({ id }))],
+    });
+
+    return await this.chatsRepository.find({
+      where: {
+        id: chat.id,
+      },
+    });
+  }
 }
