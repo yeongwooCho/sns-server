ecac13d1b1bf332fdecab8bfd4ad9c48c82e0bde
yeongwoo <jyy0223@naver.com>
2024-04-01 01:08:04 +0900

    [feat] message paginate 구현: paginate 구현을 위한 dto 생성

--- src/chats/messages/messages.service.ts
+++ src/chats/messages/messages.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { MessagesModel } from './entity/messages.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CommonService } from '../../common/common.service';
+import { PaginateMessageDto } from './dto/paginate-message.dto';
 
 @Injectable()
-export class MessagesService {
+export class ChatsMessagesService {
   constructor(
     @InjectRepository(MessagesModel)
     private readonly messagesRepository: Repository<MessagesModel>,
+    private readonly commonService: CommonService,
   ) {}
+
+  async paginateMessages(
+    dto: PaginateMessageDto,
+    overrideFunctions: FindManyOptions<MessagesModel>,
+  ) {
+    return this.commonService.paginate<MessagesModel>(
+      dto,
+      this.messagesRepository,
+      overrideFunctions,
+      'messages',
+    );
+  }
 }
