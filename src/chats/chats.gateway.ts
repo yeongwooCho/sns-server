3b048adb8f9de67268a223f257164d6dfd95a3ad
yeongwoo <jyy0223@naver.com>
2024-04-01 16:06:28 +0900

    [feat] ChatsGateway에서 createChat, enterChat에 Guard, ExceptionFilter, ValidationPipe 등록

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -62,6 +62,20 @@ export class ChatsGateway implements OnGatewayConnection {
     const chat = await this.chatsService.createChat(data);
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      // DTO 에서 class-validator 데코레이터를 사용하면 자동으로 유효성 검사를 수행한다.
+      // 이때 transform 옵션을 true 로 설정하면 요청이 들어올 때 자동으로 타입 변환을 수행한다.
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
+  @UseGuards(SocketBearerTokenGuard)
   @SubscribeMessage('enter_chat')
   async enterChat(
     @MessageBody() data: EnterChatDto,
@@ -83,12 +97,26 @@ export class ChatsGateway implements OnGatewayConnection {
     socket.join(data.chatIds.map((chatId) => chatId.toString()));
   }
 
-  // socket.on('send_message', (message) => { console.log(message); });
+  @UsePipes(
+    new ValidationPipe({
+      // DTO 에서 class-validator 데코레이터를 사용하면 자동으로 유효성 검사를 수행한다.
+      // 이때 transform 옵션을 true 로 설정하면 요청이 들어올 때 자동으로 타입 변환을 수행한다.
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
+  @UseGuards(SocketBearerTokenGuard)
   @SubscribeMessage('send_message')
   async sendMessage(
     @MessageBody() dto: CreateMessagesDto,
     @ConnectedSocket() socket: Socket,
   ) {
+    // socket.on('send_message', (message) => { console.log(message); });
     const chatExists = await this.chatsService.checkIfChatExists(dto.chatId);
     if (!chatExists) {
       throw new WsException({
