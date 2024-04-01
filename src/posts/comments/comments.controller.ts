5327dbbc2d20eccc2819682748893a5ff57f24ab
yeongwoo <jyy0223@naver.com>
2024-04-01 22:41:28 +0900

    [feat] comment controller endpoint가 Post에 완전히 귀속 되도록 변경

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 
-@Controller('comments')
+@Controller('posts/:postId/comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 }
